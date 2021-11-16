87ee67e563091e7ecaf1044090f4dfa1c5770965
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-11-16 16:44:54 -0300

    exemplo com boolean

---
+++ src/main/java/javaDate/Exemplo002.java
@@ -0,0 +1,23 @@
+package javaDate;
+
+import java.util.Date;
+
+public class Exemplo002 {
+    public static void main (String[] args){
+
+        Date dataNoPassado = new Date(1513124807691L);
+
+        Date dataNoFuturo = new Date(1613124807691L);
+
+        boolean isAfter = dataNoPassado.after(dataNoFuturo);
+
+        System.out.println(isAfter);
+
+        boolean isBefore = dataNoPassado.before(dataNoFuturo);
+
+        System.out.println(isBefore);
+
+
+
+    }
+}
