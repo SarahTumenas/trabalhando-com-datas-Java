f37ba64494fdd13afb368746a9f47dd8cfe538d2
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-11-16 17:06:20 -0300

    Classe Instant

---
+++ src/main/java/javaDate/Exemplo004.java
@@ -0,0 +1,16 @@
+package javaDate;
+
+import java.time.Instant;
+import java.util.Date;
+
+public class Exemplo004 {
+
+    public static void main(String[] args) {
+
+        Date dataInicio = new Date(1513124807691L);
+        System.out.println(dataInicio);
+
+        Instant instant = dataInicio.toInstant();
+        System.out.println(instant);
+    }
+}
