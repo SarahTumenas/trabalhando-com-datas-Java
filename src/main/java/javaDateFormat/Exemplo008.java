f760683bde804c623907d1d337d771fc4b34c1dc
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-11-17 10:25:46 -0300

    javaDateFormat

---
+++ src/main/java/javaDateFormat/Exemplo008.java
@@ -0,0 +1,19 @@
+package javaDateFormat;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class Exemplo008 {
+    public static void main(String[] args) {
+        Date agora = new Date();
+
+        String dateToStr = DateFormat.getDateInstance().format(agora);
+        System.out.println(dateToStr);
+
+        dateToStr = DateFormat.getDateInstance(DateFormat.LONG, DateFormat.SHORT).format(agora);
+        System.out.println(dateToStr);
+
+        dateToStr = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.SHORT).format(agora);
+        System.out.println(dateToStr);
+    }
+}
